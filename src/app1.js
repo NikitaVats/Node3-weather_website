446678298774d7ed10c8047ce9a8440d42fa7bf9
Nikita Vats <niitavats03@gmail.com>
2019-12-31 18:24:09 +0530

    Setup

--- src/app1.js
+++ src/app1.js
@@ -3,6 +3,7 @@ const path=require('path')
 const geocode=require('../../utils/geocode')
 const forecast=require('../../utils/forecast')
 const app=express()
+const port=process.env.PORT
 // for joining 2 directories 
 const publicDirecPath=path.join(__dirname,'../public')
 app.use(express.static(publicDirecPath))
@@ -41,6 +42,6 @@ app.get('/weather',(req,res)=>{
 app.get('/products',(req,res)=>{
     console.log(req.query)
 })
-app.listen(3000,()=>{
-    console.log('Server is up on port 3000')
+app.listen(port,()=>{
+    console.log('Server is up on port '+port)
 })
